92d731a447eb268f3bac28c844e4910356339702
Mati Weber <mati.weber@gmail.com>
2021-02-01 09:05:52 -0300

    done + bonus

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
+  // get(pos) {
+  // if(pos > this.items.length){
+  // throw new Error('OutOfBounds')
+  // } else {
+  // return this.items.pos === pos
+  // }
+  // }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  sum() {
+    // if(this.items.length === 0){
+    // return 0
+    // } else {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
+  // }
+  avg() {
+    return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
